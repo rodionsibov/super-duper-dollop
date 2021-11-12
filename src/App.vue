92ba1a976a705cc9fcf30ee578bc8aca98e0d8eb
Rodion Sibov <rodionsibov@gmail.com>
2021-11-12 12:56:12 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <header class="flex flex-col md:flex-row justify-between p-4 bg-gray-800 gap-2">
+  <header
+    class="flex flex-col md:flex-row justify-between p-4 bg-gray-800 gap-2"
+  >
     <nav class="flex flex-col md:flex-row gap-2">
-      <router-link class="bg-gray-700 text-white rounded p-4 hover:bg-gray-600 transition duration-200" to="/"
+      <router-link
+        class="
+          bg-gray-700
+          text-white
+          rounded
+          p-4
+          hover:bg-gray-600
+          transition
+          duration-200
+        "
+        to="/"
         ><i class="fas fa-utensils"></i>&nbsp; Home</router-link
       >
-      <router-link class="bg-gray-700 text-white rounded p-4 hover:bg-gray-600 transition duration-200" to="/products"
-        ><i class="fas fa-carrot"></i>&nbsp;  Products</router-link
+      <router-link
+        class="
+          bg-gray-700
+          text-white
+          rounded
+          p-4
+          hover:bg-gray-600
+          transition
+          duration-200
+        "
+        to="/products"
+        ><i class="fas fa-carrot"></i>&nbsp; Products</router-link
       >
-      <router-link class="bg-gray-700 text-white rounded p-4 hover:bg-gray-600 transition duration-200" to="/past-orders"
-        ><i class="fas fa-clipboard-list"></i>&nbsp;  Past Orders</router-link
+      <router-link
+        class="
+          bg-gray-700
+          text-white
+          rounded
+          p-4
+          hover:bg-gray-600
+          transition
+          duration-200
+        "
+        to="/past-orders"
+        ><i class="fas fa-clipboard-list"></i>&nbsp; Past Orders</router-link
       >
-      <router-link class="bg-gray-700 text-white rounded p-4 hover:bg-gray-600 transition duration-200" to="/about"
-        ><i class="fas fa-info"></i>&nbsp;  About</router-link
+      <router-link
+        class="
+          bg-gray-700
+          text-white
+          rounded
+          p-4
+          hover:bg-gray-600
+          transition
+          duration-200
+        "
+        to="/about"
+        ><i class="fas fa-info"></i>&nbsp; About</router-link
       >
     </nav>
-    <router-link class="bg-yellow-700 text-white rounded p-4 hover:bg-yellow-600 transition duration-200" to="/"
-      ><i class="fas fa-shopping-cart"></i>&nbsp;  Cart</router-link
+    <button
+      class="
+        bg-yellow-700
+        text-white
+        rounded
+        p-4
+        hover:bg-yellow-600
+        transition
+        duration-200
+      "
     >
+      <i class="fas fa-shopping-cart"></i>&nbsp; Cart
+    </button>
   </header>
 
   <router-view />
 </template>
 
 <style>
+.router-link-exact-active {
+  filter: sepia(1);
+}
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,6 @@
 <template>
-<h1>About</h1>
+  <h1 class="mt-5 text-center p-2">
+    Marketplace offers great deals on clothes, beauty, health and nutrition,
+    shoes, electronics, and more from over 1500 stores with one easy checkout.
+  </h1>
 </template>
